ad7dbaf654fe7bdffa75710a88e72f45c6d11f79
kenobi <sonichart@gmail.com>
2021-10-23 21:31:17 -0500

    Add files via upload

---
+++ contracts/flooredNFT.sol
@@ -0,0 +1,75 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.2;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract FlooredApe is ERC721, ERC721URIStorage, Pausable, Ownable {
+    using Counters for Counters.Counter;
+
+    Counters.Counter private _tokenIdCounter;
+    uint256 public MINT_RATE = 0.05 ether;
+    uint public MAX_SUPPLY = 50000;
+    
+    constructor() ERC721("FlooredApe", "FA") {}
+
+    function renounce() public onlyOwner {
+        renounceOwnership(); //from Ownable.sol
+    }
+
+    function pause() public onlyOwner {
+        _pause();
+    }
+
+    function unpause() public onlyOwner {
+        _unpause();
+    }
+
+    function safeMint(address to, string memory uri) public whenNotPaused payable {
+        require(_tokenIdCounter.current() < MAX_SUPPLY, "Tokens are sold out.");
+        require(msg.value >= MINT_RATE, "Not enough ether.");
+        _safeMint(to, _tokenIdCounter.current());
+        _setTokenURI(_tokenIdCounter.current(), uri); 
+        _tokenIdCounter.increment();
+    }
+
+    function ownerMint(address to, string memory uri) public onlyOwner { //for airdrop
+        _safeMint(to, _tokenIdCounter.current());
+        _setTokenURI(_tokenIdCounter.current(), uri); 
+        _tokenIdCounter.increment();
+    }
+
+    function _beforeTokenTransfer(address from, address to, uint256 tokenId)
+        internal
+        whenNotPaused
+        override
+    {
+        super._beforeTokenTransfer(from, to, tokenId);
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
+        super._burn(tokenId);
+    }
+
+    function contractURI() public pure returns (string memory) {
+        return "https://flooredape.mypinata.cloud/ipfs/QmXhYrXmaCoWxGFXfwW7YsnYGze4jFzubtPP5oWousbe1K";
+    }
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (string memory)
+    {
+        return super.tokenURI(tokenId);
+    }
+
+    
+
+
+}
---
+++ scripts/pause-mint.js
@@ -0,0 +1,55 @@
+require('dotenv').config();
+const API_URL = process.env.API_URL;
+const PUBLIC_KEY = process.env.PUBLIC_KEY;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+
+const { createAlchemyWeb3 } = require("@alch/alchemy-web3");
+const web3 = createAlchemyWeb3(API_URL);
+
+const contract = require("../artifacts/contracts/flooredNFT.sol/FlooredApe.json");
+const contractAddress = "0x4bCe72C8D60c365dB439253f2e2866416929A519";
+const nftContract = new web3.eth.Contract(contract.abi, contractAddress);
+
+async function pause()
+{
+  try{
+    const nonce = await web3.eth.getTransactionCount(PUBLIC_KEY, 'latest'); //get latest nonce
+  const tx = {
+    'from': PUBLIC_KEY,
+    'to': contractAddress,
+    'nonce': nonce,
+    'gas': 500000,
+    'maxPriorityFeePerGas': 1999999987,
+    'data': nftContract.methods.pause().encodeABI()
+  };
+  const signPromise = await web3.eth.accounts.signTransaction(tx, PRIVATE_KEY);
+  console.log(signPromise)
+
+  } catch(err) {
+    console.log(err)
+  }
+  
+}
+
+async function unpause()
+{
+  try{
+    const nonce = await web3.eth.getTransactionCount(PUBLIC_KEY, 'latest'); //get latest nonce
+  const tx = {
+    'from': PUBLIC_KEY,
+    'to': contractAddress,
+    'nonce': nonce,
+    'gas': 500000,
+    'maxPriorityFeePerGas': 1999999987,
+    'data': nftContract.methods.unpause().encodeABI()
+  };
+  const signPromise = await web3.eth.accounts.signTransaction(tx, PRIVATE_KEY);
+  console.log(signPromise)
+
+  } catch(err) {
+    console.log(err)
+  }
+  
+}
+pause();
+//unpause();
